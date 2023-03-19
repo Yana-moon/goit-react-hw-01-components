82ebe39508d0bf1be75a718bc42bc72538af2a48
Яна <112972800+Yana-moon@users.noreply.github.com>
2023-03-19 16:56:14 +0200

    added 1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import { Profile } from './Profile';
 import user from '../user.json';
+import { Statistics } from './Statistics';
+import data from '../data.json';
+import { FriendList } from './FriendList';
+import friends from '../friends.json';
+import { Transactions } from './Transactions';
+import transactions from '../transactions.json';
 
 export const App = () => {
   return (
     <div>
       <Profile profile={user}/>
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <Transactions items={transactions}/>
     </div>
   );
 };
-console(log)
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,49 @@
-export const Profile = () => {
-    return ()
+import { ProfileDiv, DescriptionDiv, ListDiv } from './Profile.styled';
+import PropTypes from 'prop-types';
+
+export const Profile = ({
+    user: {
+    avatar, 
+    username, 
+    tag, 
+    location,
+    stats
+    },
+}) => {
+    return (
+        <ProfileDiv>
+            <DescriptionDiv>
+                <img src={avatar} alt={tag}/>
+                <p className="name">{username}</p>
+                <p className="tag">@{tag}</p>
+                <p className="location">{location}</p>
+            </DescriptionDiv>
+            
+            <ListDiv>
+                <li>
+                    <span className="quantity"> {stats.followers}</span>
+                </li>
+                <li>
+                    <span className="quantity"> {stats.views}</span>
+                </li>
+                <li>
+                    <span className="quantity"> {stats.likes}</span>
+                </li>
+            </ListDiv>
+        </ProfileDiv>
+    );
+};
+
+Profile.propTypes = {
+    user: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+    }).isRequired,
+    }).isRequired,
 };
