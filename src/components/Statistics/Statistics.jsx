8e636e9000916bbeea1a0d9712f3dfd04013e654
Яна <112972800+Yana-moon@users.noreply.github.com>
2023-03-21 11:11:34 +0200

    added task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,20 @@
+import { StatisticsWrapper, StatisticsTitle, Statisticsdiv, StatisticsList } from './Statistics.styled.js';
+//import PropTypes from 'prop-types';
 
 
-
-export const Statistics = () => {
-    return 
-        //<>
-        //<span className="label">{label}</span>
-        //<span className="percentage">{percentage}%</span>
-        //</>
-    }
+export const Statistics = ({ title, stats}) => {
+    return (
+<StatisticsWrapper>   
+{title && <StatisticsTitle>{title}</StatisticsTitle>}
+<Statisticsdiv>
+    {stats.map((stat) =>
+    <StatisticsList key={stat.id}>
+    <span className="label">{stat.label}</span>  
+    <span className="percentage">{stat.percentage}%</span>
+    </StatisticsList>
+    )}
+</Statisticsdiv>
+</StatisticsWrapper>
+);
+};
 
---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const StatisticsWrapper = styled.div`
+display:flex;
+align-items:center;
+flex-direction:column;
+margin-left:auto;
+margin-right:auto;
+margin-top: 50px;
+width: 300px;
+height: max-content;
+border: 2px solid #FF69B4;
+border-radius: 3px;
+cursor: pointer;
+`;
+export const StatisticsTitle = styled.h2`
+color:#808080;
+margin-top:20px;
+text-transform:uppercase;
+`;
+export const Statisticsdiv = styled.ul`
+display:flex;
+width:100%;
+margin-top:20px;
+justify-content:space-between;
+`;
+export const StatisticsList = styled.li`
+display:flex;
+flex-direction:column;
+align-items:center;
+width:100%;
+padding-top:20px;
+padding-bottom:20px;
+background-color: ${getRandomHexColor};
+font-weight:500;
+border-radius:3px;
+`;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
